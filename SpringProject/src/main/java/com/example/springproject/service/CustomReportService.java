d6ad5b91c41a04595e8140b81e94b472768345c8
lee <jinwooklee0916@skuniv.ac.kr>
2025-09-23 14:37:32 +0900

    Remove SpringProject module

--- SpringProject/src/main/java/com/example/springproject/service/CustomReportService.java
+++
@@ -1,7 +0,0 @@
-package com.example.springproject.service;
-
-import com.example.springproject.model.CustomReport;
-
-public interface CustomReportService {
-    CustomReport getCustomReportData(String userId);
-}
